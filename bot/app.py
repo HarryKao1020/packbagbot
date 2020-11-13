a72aad148aa33d830274df6966aee38f05eea6bd
10646028 <10646028@ntub.edu.tw>
2020-11-14 00:09:39 +0800

    history追加URL

--- bot/app.py
+++ bot/app.py
@@ -296,8 +296,8 @@ def error(update, context):
 #=================history_conv==================
 def history(bot, update):        #/history 查詢歷史行程
     UserID = update.message.from_user['id']
-
     Tnames = db.getTnames([UserID]) #出來是 tunlp ex:[('name1',),('name2',)]
+    
     if Tnames:
         reply = '這是你過去安排的行程:\n'
         keyboard = []
@@ -306,11 +306,13 @@ def history(bot, update):        #/history 查詢歷史行程
             keyboard.append([InlineKeyboardButton(Tname[0], callback_data=Tname[0])],)
 
         reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_text(reply,reply_markup=reply_markup)
+        update.message.reply_text(reply, reply_markup = reply_markup)
+
     else:
         reply = '你還沒有安排拉'
         update.message.reply_text(reply)
         return ConversationHandler.END
+
     return HISTORY
 
 def history_output(bot, update): #/history 查詢歷史行程：列出歷史行程的景點
@@ -318,17 +320,21 @@ def history_output(bot, update): #/history 查詢歷史行程：列出歷史行�
     UserID = query.from_user['id']
     Tname = query.data
     
-    landmarks = db.getPLACE([UserID,Tname])
+    landmarks = db.getPLACE([UserID, Tname])
     i = 1
     place_output = ""
     for landmark in landmarks:
         if landmark:
-            place_output += str(i) +". "+landmark + "\n"
+            place_output += str(i) + ". " + landmark + "\n"
             i += 1
         else:
             break
+    
+    history_URL = 'http://127.0.0.1:5000/' + str(UserID) + '/' + Tname
 
-    query.edit_message_text(place_output)
+    query.edit_message_text(history_URL)
+    query.edit_message_text(place_output +"\n" + history_URL)
+
     return ConversationHandler.END
 
 #===================================================================
@@ -839,13 +845,14 @@ def done(bot,update):
         else:
             break
 
-    webUrl = '/' + str(UserID) + '/' + travelname[UserID]
+    webUrl = 'http://127.0.0.1:5000/' + str(UserID) + '/' + travelname[UserID]
 
     update.message.reply_text('旅泊包幫你安排好行程嘍')
     update.message.reply_text(place_output)
-    update.message.reply_text('http://127.0.0.1:5000' + webUrl)
+    update.message.reply_text(webUrl)
     update.message.reply_text('希望你喜歡旅泊包安排的行程🐾\n祝你玩得愉快！')
-    print('http://127.0.0.1' + webUrl )
+
+    print(webUrl)
 
     getWeather(tmpcounty[UserID], update)
     
